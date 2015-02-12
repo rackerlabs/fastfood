09b0451fa675bfa4b38cc52b82000474af935161
Jim Rosser <jarosser06@gmail.com>
2015-02-12 10:14:57 -0600

    renamed stencil class to stencil set

--- fastfood/pack.py
+++ fastfood/pack.py
@@ -10,7 +10,7 @@ class TemplatePack(object):
     def __init__(self, path):
         """Initialize, asserting templatepack path and manifest."""
         self._manifest = None
-        self._stencils = {}
+        self._stencil_sets = {}
         self.path = utils.normalize_path(path)
         if not os.path.isdir(path):
             raise ValueError("Templatepack dir %s does not exist."
@@ -40,39 +40,56 @@ class TemplatePack(object):
         return self._manifest
 
     @property
-    def stencils(self):
-        return self._stencils
+    def stencil_sets(self):
+        return self._stencil_sets
 
-    def __getattr__(self, stencil_name):
+    def __getattr__(self, stencilset_name):
         """Shortcut to self.load_stencil()."""
-        return self.load_stencil(stencil_name)
+        return self.load_stencil(stencilset_name)
 
-    def load_stencil(self, stencil_name):
-        """Return the Stencil from this template pack."""
-        if stencil_name not in self._stencils:
-            if stencil_name not in self.manifest['stencil_sets'].keys():
+    def load_stencil_set(self, stencilset_name):
+        """Return the Stencil Set from this template pack."""
+        if stencilset_name not in self._stencil_sets:
+            if stencilset_name not in self.manifest['stencil_sets'].keys():
                 raise AttributeError("Stencil set not listed in %s under "
                                      "stencil_sets." % self.manifest_path)
             stencil_path = os.path.join(
-                self.path, 'stencils', stencil_name)
+                self.path, 'stencils', stencilset_name)
             try:
-                self._stencils[stencil_name] = Stencil(stencil_path)
+                self._stencil_sets[stencilset_name] = StencilSet(stencil_path)
             except ValueError as err:
                 raise AttributeError(err.message)
-        return self._stencils[stencil_name]
+        return self._stencil_sets[stencilset_name]
 
 
-class Stencil(object):
+class StencilSet(object):
 
     def __init__(self, path):
         self.path = utils.normalize_path(path)
         if not os.path.isdir(path):
-            raise ValueError("Stencil dir %s does not exist." % path)
+            raise ValueError("Stencil Set dir %s does not exist." % path)
         self.manifest_path = os.path.join(self.path, 'manifest.json')
         if not os.path.isfile(self.manifest_path):
-            raise ValueError("Stencil needs manifest file, %s"
+            raise ValueError("Stencil Set needs manifest file, %s"
                              % self.manifest_path)
+        self._validate('api', cls=int)
+        self._validate('default_stencil', cls=str)
         self._manifest = None
+        self._stencils = {}
+
+    def _validate(self, key, cls=None):
+        if key not in self.manifest:
+            raise ValueError("Stencil Set %s requires '%s'."
+                             % (self.manifest_path, key))
+        if cls:
+            if not isinstance(self.manifest[key], cls):
+                raise TypeError("Stencil Set value '%s' should be %s, not %s"
+                                % (key, cls, type(self.manifest[key])))
+
+    def load_stencils(self):
+        if self.manifest.get('stencils'):
+            self._stencils = self.manifest.get('stencils')
+        return self._stencils
 
     @property
     def manifest(self):
@@ -81,5 +98,10 @@ class Stencil(object):
                 self._manifest = json.load(man)
         return self._manifest
 
-    def recipes(self):
+    @property
+    def stencils(self):
+        return self._stencils
+
+    @property
+    def options(self, stencil):
         pass
