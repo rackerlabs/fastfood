dafd8a090af869235987c6a932c13bd64d19a664
Sam Stavinoha <smlstvnh@gmail.com>
2015-02-12 19:38:16 -0600

    ruby_lines() util

--- fastfood/utils.py
+++ fastfood/utils.py
@@ -14,3 +14,17 @@ def normalize_path(path):
 def ruby_strip(chars):
     """Strip whitespace and any quotes."""
     return chars.strip(' "\'')
+
+
+def ruby_lines(text):
+    """Tidy up lines from a file, honor # comments.
+
+    Does not honor ruby block comments (yet).
+    """
+    if isinstance(text, basestring):
+        text = text.splitlines()
+    elif not isinstance(text, list):
+        raise TypeError("text should be a list or a string, not %s"
+                        % type(text))
+    return [l.strip() for l in text if l.strip()
+            and not l.strip().startswith('#')]
