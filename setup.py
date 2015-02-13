1b753460852d2d1576da32e3101f183690349f6c
Sam Stavinoha <smlstvnh@gmail.com>
2015-02-13 17:26:21 -0600

    add license headers

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-import re
-import ast
+#!/usr/bin/env python
+
+# Copyright 2015 Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Fastfood packaging and installation."""
+
 from setuptools import setup, find_packages
 
 
-#_version_re = re.compile(r'__version__\s+=\s+(.*)')
-#_url_re = re.compile(r'__url__\s+=\s+(.*)')
-
-
-#with open('fastfood/__init__.py', 'rb') as f:
-#    f = f.read()
-#    version = str(ast.literal_eval(_version_re.search(
-#        f.decode('utf-8')).group(1)))
-#    url = str(ast.literal_eval(_url_re.search(
-#        f.decode('utf-8')).group(1)))
-
-
-dependencies = [
+DEPENDENCIES = [
     'jinja2==2.7.3',
 ]
-tests_require = [
+TESTS_REQUIRE = [
 ]
 
 setup(
@@ -27,9 +31,9 @@ setup(
     keywords='...',
     version='1.0',
     url='https://github.com/rackerlabs/fastfood',
-    tests_require=tests_require,
+    tests_require=TESTS_REQUIRE,
     test_suite='tests',
-    install_requires=dependencies,
+    install_requires=DEPENDENCIES,
     packages=find_packages(exclude=['tests']),
     classifiers=[
         "Programming Language :: Python",
