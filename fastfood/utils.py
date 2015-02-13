6faed463d4986573a3c73d40360eb6cb4ae1cb88
Sam Stavinoha <smlstvnh@gmail.com>
2015-02-12 19:38:29 -0600

    deepupdate() recursive dict update

--- fastfood/utils.py
+++ fastfood/utils.py
@@ -28,3 +28,28 @@ def ruby_lines(text):
                         % type(text))
     return [l.strip() for l in text if l.strip()
             and not l.strip().startswith('#')]
+
+
+def deepupdate(original, update, levels=5):
+    """Update, like dict.update, but deeper.
+
+    Update 'original' from dict/iterable 'update'.
+
+    I.e., it recurses on dicts 'levels' times if necessary.
+
+    A standard dict.update is levels=0
+    """
+    if not isinstance(update, dict):
+        update = dict(update)
+    if not levels > 0:
+        original.update(update)
+    else:
+        for key, val in update.iteritems():
+            if isinstance(original.get(key), dict):
+                # might need a force=True to override this
+                if not isinstance(val, dict):
+                    raise TypeError("Trying to update dict %s with "
+                                    "non-dict %s" % (original[key], val))
+                deepupdate(original[key], val, levels=levels-1)
+            else:
+                original.update({key: val})
