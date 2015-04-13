420c5cd1b453f0cf75884529bb11815dedf7da3a
Sam Stavinoha <smlstvnh@gmail.com>
2015-04-12 22:18:14 -0500

    add apache and nodejs test stencils

---
+++ tests/test_templatepack/stencils/nodejs/recipes/_nodejs.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: |{ cookbook['name'] }|
+# Recipe :: |{ options['name'] }|
+#
+# Copyright |{ cookbook['year'] }|, Rackspace
+#
+
+# see https://github.com/erulabs/nodejs for usage
+include_recipe 'nodejs'
